35f77dcd1ec5b7e06bbaf7ad86464f565d54e0d8
Charles Muchogo <muchpaul2@gmail.com>
2024-11-18 19:53:27 +0300

    Added created at in timeusers table

--- models/user.go
+++ models/user.go
@@ -2,23 +2,25 @@ package models
 
 import (
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 type User struct {
-	ID             uint   `gorm:"primaryKey;autoIncrement:true;unique" json:"id"`
-	FirstName      string `json:"firstName"`
-	LastName       string `json:"lastName"`
-	Phone          string `gorm:"unique" json:"phone"`
-	Email          string `json:"email" gorm:"unique"`
-	Password       string `json:"-"`
-	DeviceId       string `json:"-"`
-	ProfilePhoto   string `json:"profilePhoto"`
-	Age            string `json:"age"`
-	EducationLevel string `json:"educationLevel"`
-	TestedBefore   bool   `json:"testedBefore"`
-	SaveResults    bool   `json:"SaveResults"`
-	Gender         string `json:"gender"`
-	Role           string `json:"role"`
+	ID             uint      `gorm:"primaryKey;autoIncrement:true;unique" json:"id"`
+	FirstName      string    `json:"firstName"`
+	LastName       string    `json:"lastName"`
+	Phone          string    `gorm:"unique" json:"phone"`
+	Email          string    `json:"email" gorm:"unique"`
+	Password       string    `json:"-"`
+	DeviceId       string    `json:"-"`
+	ProfilePhoto   string    `json:"profilePhoto"`
+	Age            string    `json:"age"`
+	EducationLevel string    `json:"educationLevel"`
+	TestedBefore   bool      `json:"testedBefore"`
+	SaveResults    bool      `json:"SaveResults"`
+	Gender         string    `json:"gender"`
+	Role           string    `json:"role"`
+	CreatedAt      time.Time `gorm:"autoCreateTime" json:"createdAt"`
 }
 
 func (user *User) HashPassword(password string) error {
