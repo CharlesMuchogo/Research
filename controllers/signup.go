b658960babd465ac4238f90ff08ba584e6458201
charles <muchpaul2@gmail.com>
2025-07-19 16:37:02 +0300

    chore: clean up update user details endpoint

--- controllers/signup.go
+++ controllers/signup.go
@@ -6,6 +6,7 @@ import (
 	"awesomeProject/fcm"
 	"awesomeProject/models"
 	"awesomeProject/models/dto"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -68,10 +69,6 @@ func RegisterUser(context *gin.Context) {
 
 func UpdateUserDetails(context *gin.Context) {
 	var user models.User
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	var existingUser models.User
 
 	if err := context.ShouldBindJSON(&user); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
@@ -80,43 +77,24 @@ func UpdateUserDetails(context *gin.Context) {
 	}
 
 	token := context.GetHeader("Authorization")
-
 	userFromToken, _ := auth.GetUserDetailsFromToken(token)
 
-	if err := database.DbInstance.Where("email = ?", userFromToken.Email).Find(&existingUser).Error; err != nil {
-
+	if err := database.DbInstance.Model(&models.User{}).
+		Where("email = ?", userFromToken.Email).
+		Select("TestedBefore", "Age", "EducationLevel", "Gender").
+		Updates(user).Error; err != nil {
+		fmt.Printf("Error updating user %s", err.Error())
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to update details"})
-		context.Abort()
 		return
 	}
 
-	if existingUser.Email == "" {
-		context.JSON(http.StatusNotFound, gin.H{"message": "Profile not found"})
-		context.Abort()
+	var updatedUser models.User
+	if err := database.DbInstance.
+		Where("email = ?", userFromToken.Email).
+		First(&updatedUser).Error; err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch updated user"})
 		return
 	}
 
-	user.ID = existingUser.ID
-	user.Email = existingUser.Email
-	user.Phone = existingUser.Phone
-	user.Password = existingUser.Password
-	user.FirstName = existingUser.FirstName
-	user.LastName = existingUser.LastName
-	user.ProfilePhoto = existingUser.ProfilePhoto
-	user.Country = existingUser.Country
-	user.CreatedAt = existingUser.CreatedAt
-
-	if err := database.DbInstance.Save(&user).Error; err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update user details"})
-		context.Abort()
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{"message": "Details updated successfully", "user": user})
-
-	go func() {
-		defer wg.Done()
-		fcm.RegisterTopic(user.Email, user.DeviceId)
-	}()
-	wg.Wait()
+	context.JSON(http.StatusOK, gin.H{"message": "Details updated successfully", "user": updatedUser})
 }
